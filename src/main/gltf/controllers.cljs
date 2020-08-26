bcecb6f035e7464a2e0b27214a470d0b7be061c2
Carmen Wick <CarmenWick@gmail.com>
2020-08-26 00:47:48 -0700

    More responsive controls when changing direction

--- src/main/gltf/controllers.cljs
+++ src/main/gltf/controllers.cljs
@@ -6,25 +6,40 @@
     {:yaw-delta (* dx sensitivity)
      :pitch-delta (* dy sensitivity)}))
 
-(def SENSITIVITY 2)
-(def DRAG 3)
+(def FORCE 2)
+(def COUNTER-FORCE 3)
 
 (defn- get-input-axis-value [current user-impulse time]
-  (let [impulse
+  (let [; Pick a direction to apply an input force.
+        ; If there's no user input on this axis we'll apply a force in the opposite
+        ; direction to start returning the input position to neutral
+        impulse
         (if (zero? user-impulse)
           (- (js/Math.sign current))
           user-impulse)
 
+        ; Pick between a counter-force and normal force
+        ; Normal: Current position is zero, or in the same direction the user is already applying force
+        ; Counter: User is applying force in the opposite direction from current input, or 
+        ;   has let go of the controls on this axis.
+        ; Having two different values here allows for better fine tuning. Having the counter force
+        ;   higher than the normal force feels more responsive.
         acceleration
-        (if (zero? user-impulse)
-          DRAG
-          SENSITIVITY)
+        (if (or (zero? user-impulse)
+                (and
+                 (not (zero? current))
+                 (not= (js/Math.sign user-impulse) (js/Math.sign current))))
+          COUNTER-FORCE
+          FORCE)
 
         clamp #(max -1 (min 1 %))
 
         new-value
         (+ current (* acceleration impulse time))]
     (clamp
+     ; We need to detect if the force we're applying would cause the input value to flip
+     ; to the other side of the axis. If that happens, we return the input to neutral
+     ; so it doesn't go flying off in the other direction.
      (if (and (zero? user-impulse) (not= (js/Math.sign new-value) (js/Math.sign current)))
        0
        new-value))))
